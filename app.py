7527d3b855b5deddd994935ce4b3229fd741a5f8
akshay-404 <akshayanilparavur@gmail.com>
2025-10-24 01:41:56 +0530

    fixed db issue

--- app.py
+++ app.py
@@ -12,7 +12,9 @@ import os
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-    app.config["DATABASE_URL"] = os.getenv("DATABASE_URL")
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     db.init_app(app)
     login_manager.init_app(app)
     bcrypt.init_app(app)
